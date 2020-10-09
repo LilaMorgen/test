995165dc2d3c4c30b746238ba9b2ce2f5d4a4999
Lila Morgen <lilamorgen@163.com>
2020-10-09 17:39:15 +0800

    注释

--- window_app/runwindow.py
+++ window_app/runwindow.py
@@ -10,11 +10,17 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 def _main():
+    # 创建应用程序
     app = QApplication(sys.argv)
+    # 创建主窗口
     main_window = QMainWindow()
+    # 创建控件实例
     ui = demo.Ui_MainWindow()
+    # 绘制窗口中的控件
     ui.setupUi(main_window)
+    # 循环显示主窗口
     main_window.show()
+    # 退出循环命令
     sys.exit(app.exec_())
 
 
