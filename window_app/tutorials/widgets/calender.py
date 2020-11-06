a02b72105a263f68c126d30275d3ace518ea0078
zero <zero@123.com>
2020-11-06 18:13:30 +0800

    Calender Widget

--- window_app/tutorials/widgets/calender.py
+++ window_app/tutorials/widgets/calender.py
@@ -16,20 +16,30 @@ class Example(QWidget):
         self.init_ui()
 
     def init_ui(self):
+        # 创建垂直布局
         v_box = QVBoxLayout(self)
 
+        # 创建日历对象
         cal = QCalendarWidget(self)
+        # 显示日历网格
         cal.setGridVisible(True)
+        # 将日历上的QDate信号与槽相连
         cal.clicked[QDate].connect(self.show_date)
 
+        # 在垂直布局中添加日历
         v_box.addWidget(cal)
 
+        # 创建标签对象
         self.lbl = QLabel(self)
+        # 获取当前日历选择的日期
         date = cal.selectedDate()
+        # 在标签上展示日期
         self.lbl.setText(date.toString())
 
+        # 在垂直布局中加入标签对象
         v_box.addWidget(self.lbl)
 
+        # 将垂直布局放置在窗口上
         self.setLayout(v_box)
 
         self.setGeometry(300, 300, 350, 300)
