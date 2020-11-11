1efa7d6fc06ac2de4a684a007c4a07df1b9f82bc
Lila Morgen <lilamorgen@163.com>
2020-11-11 23:55:47 +0800

    toolbar and icons

--- window_app/simple_gui_application/basic_qt_features/toolbars_and_menus.py
+++ window_app/simple_gui_application/basic_qt_features/toolbars_and_menus.py
@@ -23,9 +23,36 @@ class MainWindow(QMainWindow):
         self.windowTitleChanged.connect(lambda x: self.my_custom_fn(x, 25))
 
         self.setWindowTitle('My Awesome App')
+        self.setGeometry(300, 300, 300, 220)
+
         lbl = QLabel('This is Awesome!!!')
         lbl.setAlignment(Qt.AlignCenter)
         self.setCentralWidget(lbl)
+
+        toolbar = QToolBar('My Main Toolbar')
+        # 设置工具栏图标大小
+        self.setIconSize(QSize(16, 16))
+        # 设置工具栏按钮风格
+        self.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        self.addToolBar(toolbar)
+
+        statusbar = QStatusBar(self)
+        self.setStatusBar(statusbar)
+
+        button_action = QAction(QIcon('../icons/16/045.png'),  'My Action', self)
+        button_action.setStatusTip('This is action.')
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        button_action.setCheckable(True)
+        toolbar.addAction(button_action)
+
+        toolbar.addSeparator()
+
+        toolbar.addWidget(QLabel('Hello'))
+        toolbar.addSeparator()
+        toolbar.addWidget(QCheckBox())
+
+    def onMyToolBarButtonClick(self, s):
+        print('click', s)
 
     def onWindowTitleChanged(self, s):
         print(s)
