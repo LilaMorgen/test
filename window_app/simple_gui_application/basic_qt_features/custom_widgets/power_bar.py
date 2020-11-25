c3a5a04848cba1cfc5cd8aa249ecb75490988106
zero <zero@123.com>
2020-11-25 18:28:39 +0800

    bar connect dial

--- window_app/simple_gui_application/basic_qt_features/custom_widgets/power_bar.py
+++ window_app/simple_gui_application/basic_qt_features/custom_widgets/power_bar.py
@@ -27,6 +27,25 @@ class _Bar(QWidget):
         rect = QRect(0, 0, painter.device().width(), painter.device().height())
         painter.fillRect(rect, brush)
 
+        dial = self.parent()._dial
+        vmin, vmax = dial.minimum(), dial.maximum()
+        value = dial.value()
+
+        pen = painter.pen()
+        pen.setColor(QColor('red'))
+        painter.setPen(pen)
+
+        font = painter.font()
+        font.setFamily('Times')
+        font.setPointSize(18)
+        painter.setFont(font)
+
+        painter.drawText(25, 25, '{}---{}---{}'.format(vmin, value, vmax))
+        painter.end()
+
+    def _trigger_refresh(self):
+        self.update()
+
 
 class PowerBar(QWidget):
     """
@@ -40,6 +59,7 @@ class PowerBar(QWidget):
         layout.addWidget(self._bar)
 
         self._dial = QDial()
+        self._dial.valueChanged.connect(self._bar._trigger_refresh)
         layout.addWidget(self._dial)
 
         self.setLayout(layout)
